980dce8d8eed23b371cb98cdb603e6e125eb864a
Aljosa <asrebo@gmail.com>
2025-12-02 02:33:40 +0100

    Add Docker support and standalone Next.js output

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-   images: {
+  images: {
     remotePatterns: [new URL('https://image.tmdb.org/**')],
   },
   reactCompiler: true,
+  output: 'standalone',
 };
 
 export default nextConfig;
